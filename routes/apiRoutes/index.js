1f9cdbae9d79183c287e6bd07c2df9e30f9ef744
Vinay Vallabhaneni <vinssmedia@gmail.com>
2022-01-30 08:54:03 -0500

    routes for employee

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.use(require('./employeeRoutes'));
+router.use(require('./roleRoutes'));
+router.use(require('./departmentRoutes'));
+
+
+module.exports = router;
